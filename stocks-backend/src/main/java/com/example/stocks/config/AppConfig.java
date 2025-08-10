2055ac7725c010798c14178922cf55216d050845
Ionut <ionut@enthuse.com>
2025-08-10 16:36:35 +0100

    Fixing test failures and performing a Spotless apply

--- stocks-backend/src/main/java/com/example/stocks/config/AppConfig.java
+++ stocks-backend/src/main/java/com/example/stocks/config/AppConfig.java
@@ -1,9 +1,5 @@
 package com.example.stocks.config;
 
-import com.example.stocks.component.PriceGenerator;
-import com.example.stocks.component.PriceGeneratorImpl;
-import com.example.stocks.service.StockService;
-import com.example.stocks.service.StockServiceImpl;
 import java.time.Clock;
 import java.util.Random;
 import org.springframework.context.annotation.Bean;
@@ -21,5 +17,4 @@ public class AppConfig {
   public Random random() {
     return new Random();
   }
-
 }
